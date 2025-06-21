b18b734e68e4cebb6e9ebf641ec514f6fdb9e0ff
Vladislav <gluhovvladislav514@gmail.com>
2025-06-21 06:11:43 +0300

    Зачистка

--- main copy.py
+++ main copy.py
@@ -1,12 +1,49 @@
+import logging
+
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 
 from modules.paths import build_paths, ensure_dirs_exist
+from log_filters import ErrorLogFilter, CriticalLogFilter, DebugWarningLogFilter
 from modules.processor import LoanProcessor
 
 
+def configure_logging():
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    formatter_1 = logging.Formatter(
+        fmt='[%(asctime)s] #%(levelname)-8s %(filename)s:'
+            '%(lineno)d - %(name)s:%(funcName)s - %(message)s'
+    )
+
+    formatter_2 = logging.Formatter(
+        fmt='[%(asctime)s] #%(levelname)-8s - %(message)s'
+    )
+
+    error_file = logging.FileHandler('error.log', 'w', encoding='utf-8')
+    error_file.setLevel(logging.DEBUG)
+    error_file.addFilter(ErrorLogFilter())
+    error_file.setFormatter(formatter_1)
+
+    stdout = logging.StreamHandler(sys.stdout)
+    stdout.addFilter(DebugWarningLogFilter())
+    stdout.setFormatter(formatter_2)
+
+    stderr = logging.StreamHandler()
+    critical_file = logging.FileHandler('critical.log', mode='w', encoding='utf-8')
+    critical_file.setFormatter(fmt=formatter_1)
+    critical_file.addFilter(CriticalLogFilter())
+
+    logger.addHandler(stderr)
+    logger.addHandler(critical_file)
+    logger.addHandler(stdout)
+    logger.addHandler(error_file)
+
+
 def get_driver_kwargs() -> dict:
+    configure_logging()
     opts = Options()
     opts.add_argument("--window-size=1200,800")
     opts.add_argument(
--- text_appel.py
+++
@@ -1,4 +0,0 @@
-
-def get_text(locality: str, fio: str, birthday: str) -> str:
-    return f"""Прошу рассмотреть заявление {locality} в отношении {fio} {birthday}
-"""
